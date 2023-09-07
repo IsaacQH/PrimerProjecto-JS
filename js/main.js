6ff21ce8ef6e68a79b8e8e63ec435a4b488fcb9c
IsaacQH <isaacqwert@gmail.com>
2023-09-06 18:13:02 -0600

    Se creó slider resposivo

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){                //Carga el código JS
+
+    $('.galeria').bxSlider({
+        mode: 'fade',
+        caption: true,
+        slideWidth: 1400,
+        resposive: true,
+        pager:false
+    })
+
+
+})
