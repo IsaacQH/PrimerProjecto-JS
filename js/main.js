92fa108b8fa068316e6bf597b63a2da277345058
IsaacQH <isaacqwert@gmail.com>
2023-09-11 20:35:52 -0600

    Temas y scroll automático

--- js/main.js
+++ js/main.js
@@ -38,21 +38,44 @@ $(document).ready(function(){                //Carga el código JS
         }
     ]
 
-    
-
     posts.forEach((item, index) => {                  //Se crea ciclo forEach que revise cada item e index
         //Creamos una variable que cree plantillas JS en HTML de cada valor del item
         var post = `
             <h2>${item.title}</h2>                                          
                 <span class = "fecha">${item.date}</span>      
                 <p>
-                    ${item.content}  
-                </p>`
+                    ${item.content}
+                    <br>  
+                    <a href="#" class = "botonMore">Leer más ...</a>
+                    <div class ="clearfix" ></div>
+                </p> `
 
         $('.article').append(post)        
-        console.log(post)
-
+        //console.log(post)
     })
+
+    //SELECTOR DE TEMAS ************************************************************************************************
+    var theme = $('#theme')                          //Guarda en variable el script de temas
+    
+    $('#to-Green').click(function(){                 //Evento click al picar el div de to green
+        theme.attr("href", "css/green.css")          //Busca elemento href de theme y lo cambia a green.css
+    })
+    $('#to-Red').click(function(){                   //Evento click al picar el div de to red
+        theme.attr("href", "css/red.css")            //Busca elemento href de theme y lo cambia a red.css
+    })
+    $('#to-Blue').click(function(){                   //Evento click al picar el div de to blue
+        theme.attr("href", "css/blue.css")            //Busca elemento href de theme y lo cambia a blue.css
+    })
+
+    //SCROLL ***********************************************************************************************************
+    $('.scroll').click(function(e){                   //Crea evento click en el elemento scroll
+        e.preventDefault()                           //Nos permite que no se refreshee cuando se de click                           
+        $('body, html').animate({                  //Hace una animación en html y body
+            scrollTop: 0                             //Hace scroll hacia el pixel 0
+        },500)                                      //lo hace en 500ms
+        return false                                 //Nos permite que no se refreshee cuando se de click
+    })
+
 
 
 })
