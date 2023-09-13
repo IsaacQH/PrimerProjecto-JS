1826bb0e2a92e405f246792f762c8d978e886bfb
IsaacQH <isaacqwert@gmail.com>
2023-09-12 20:34:56 -0600

    Correciones

--- js/contacto.js
+++ js/contacto.js
@@ -10,4 +10,17 @@ $(document).ready(function(){
         lang:'es'                                     //Idioma español
     })
 
+    //SELECTOR DE TEMAS ************************************************************************************************
+    var theme = $('#theme')                          //Guarda en variable el script de temas
+    
+    $('#to-Green').click(function(){                 //Evento click al picar el div de to green
+        theme.attr("href", "css/green.css")          //Busca elemento href de theme y lo cambia a green.css
+    })
+    $('#to-Red').click(function(){                   //Evento click al picar el div de to red
+        theme.attr("href", "css/red.css")            //Busca elemento href de theme y lo cambia a red.css
+    })
+    $('#to-Blue').click(function(){                   //Evento click al picar el div de to blue
+        theme.attr("href", "css/blue.css")            //Busca elemento href de theme y lo cambia a blue.css
+    })
+
 })
--- js/reloj.js
+++ js/reloj.js
@@ -6,4 +6,17 @@ $(document).ready(function(){
         $('#reloj').html("Time: "+ reloj)                                 // escribe en html div reloj la variable reloj
     },1000)
 
+    //SELECTOR DE TEMAS ************************************************************************************************
+    var theme = $('#theme')                          //Guarda en variable el script de temas
+    
+    $('#to-Green').click(function(){                 //Evento click al picar el div de to green
+        theme.attr("href", "css/green.css")          //Busca elemento href de theme y lo cambia a green.css
+    })
+    $('#to-Red').click(function(){                   //Evento click al picar el div de to red
+        theme.attr("href", "css/red.css")            //Busca elemento href de theme y lo cambia a red.css
+    })
+    $('#to-Blue').click(function(){                   //Evento click al picar el div de to blue
+        theme.attr("href", "css/blue.css")            //Busca elemento href de theme y lo cambia a blue.css
+    })
+
 })
